a88b1b22c980b4c5290c6a99353ede8f7ca1dd98
Jake Nagel <jdnagel1@gmail.com>
2019-07-10 15:06:21 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# _Ping Pong_
+
+#### By _**Jake**_
+
+## Description
+
+|Description|Input|Output|
+|-|-|-|
+|Will print a range of numbers from 0 to the input|4|0, 1, 2, 3, 4|
+|Numbers divisible by 3 become "ping"|3|"ping"
+|Numbers divisible by 5 become "pong"|5|"pong"
+|Numbers divisible by 15 become "pingpong"|15|"pingpong"
+
+
+
+## Setup/Installation Requirements
+
+* _Run in browser_
+
+## Known Bugs
+
+_No known bugs._
+
+## Support and contact details
+
+_jdnagel1@gmail.com_
+
+## Technologies Used
+
+_JS_
+
+### License
+
+Copyright <2019> <Jake>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ js/scripts.js
@@ -0,0 +1,102 @@
+// Business Logic
+// Starts new pizza order
+function PizzaOrder() {
+  this.pizzas = [],
+  this.currentId = 0
+};
+// Starts new individual pizza
+function Pizza(toppings, size) {
+  this.toppings = toppings,
+  this.size = size,
+  this.price = 10
+};
+// Assign each pizza an Id
+PizzaOrder.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+// Add pizza to order
+PizzaOrder.prototype.addPizza = function(pizza) {
+  pizza.id = this.assignId();
+  this.pizzas.push(pizza);
+};
+// Add size of pizza
+Pizza.prototype.addSize = function(size) {
+  this.size = size;
+}
+// Add toppings to pizza
+Pizza.prototype.addToppings = function(toppings) {
+  this.toppings = toppings;
+};
+// Pizza price by size options
+Pizza.prototype.priceBySize = function(size) {
+  this.size = size;
+  if(this.size === "large"){
+    this.price;
+  } else if (this.size === "extraLarge") {
+    this.price += 4;
+  } else if (this.size === "personalPizza") {
+    this.price += 7;
+  } else {
+    console.log("There is an error for pizza size selection");
+  }
+};
+// Pizza price by toppings
+Pizza.prototype.priceByToppings = function(topping) {
+  if(topping === "extraCheese") {
+    this.price += 1;
+  } else if(topping === "pepperoni") {
+    this.price += 1;
+  } else if (topping === "sausage") {
+    this.price -= 1;
+  } else if(topping === "ham") {
+    this.price -= 1;
+  }  else if (topping === "bellpeppers") {
+    this.price += 1;
+  } else if(topping === "onions") {
+    this.price += 1;
+  } else if (topping === "mushrooms") {
+    this.price  += 1;
+  } else if (topping === "") {
+    this.price;
+  } else {
+    console.log("There is an error for pizza topping selection");
+  }
+}
+// turn topping camelCase into text
+Pizza.prototype.arrayDisplay = function() {
+  this.toppings.toString();
+}
+// User Interface Logic
+$(document).ready(function() {
+  var pizzaOrder = new PizzaOrder();
+  $("button#pizzaButton").click(function(event) {
+    event.preventDefault();
+    $("button#pizzaButton").hide();
+    $("form.pizzaOptions").show();
+  });
+  $("form.pizzaOptions").submit(function(event) {
+    event.preventDefault();
+    checked = $("input[type=checkbox]:checked").length;
+      if(!checked) {
+        alert("You must check at least one option");
+        return false;
+      }
+    var pizza = new Pizza();
+    var size = $(this.size).val();
+    pizza.priceBySize(size);
+    pizzaOrder.addPizza(pizza);
+    var toppings = [];
+    $.each($("input:checkbox[name=topping]:checked"), function() {
+      toppings.push($(this).val());
+    });
+    pizza.addToppings(toppings);
+    for (var i = 0; i < pizza.toppings.length; i++) {
+      pizza.priceByToppings(toppings[i]);
+    };
+    $("form.pizzaOptions").hide();
+    $("div.checkout").show();
+    $("h4.penultimateTransmisssion").append("You ordered a " + $("#size").children("option:selected").text() + " pizza with " + pizza.toppings.toString() + ".");
+    $("h4.finalTransmisssion").append("Your order total will be $" + pizza.price + ".");
+  });
+});
